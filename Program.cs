b8f87a26ed9b1500ca404e74aa265e3f29653d01
b1-loop <bojidarivanov98@gmail.com>
2025-09-02 11:12:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace inspelning_visual_studio_genomgång
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ bozhi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inspelning_visual_studio_genomgång
+{
+    internal class bozhi
+    {
+    }
+}
